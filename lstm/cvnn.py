182a58f0c267a51862ee207a7a426848dbfe9126
Lukas <lukas.petersson.1999@gmail.com>
2022-07-21 22:07:30 +0200

    retrain cvnn

--- lstm/cvnn.py
+++ lstm/cvnn.py
@@ -15,8 +15,9 @@ SEQ_LEN = 5
 
 ##
 class Dataset(torch.utils.data.Dataset):
-    def __init__(self, data_dir, seq_len=SEQ_LEN):
-        self.data = np.load(data_dir, allow_pickle=True)
+    def __init__(self, data_size=54, seq_len=SEQ_LEN):
+        self.data = np.array(np.concatenate([np.load('data/data_big_'+str(i*100)+'.npy', allow_pickle=True) for i in range(1,data_size)]))
+
         self.seq_len = seq_len
 
     def data_in_part(self, part):
@@ -57,7 +58,8 @@ class CvnnModel(nn.Module):
         super(CvnnModel, self).__init__()
         self.l1 = nn.Linear(layer_dims[0], layer_dims[1]).to(torch.cfloat)
         self.l2 = nn.Linear(layer_dims[1], layer_dims[2]).to(torch.cfloat)
-        self.l3 = nn.Linear(layer_dims[2], 1).to(torch.cfloat)
+        self.l3 = nn.Linear(layer_dims[2], layer_dims[3]).to(torch.cfloat)
+        self.l4 = nn.Linear(layer_dims[3], 1).to(torch.cfloat)
         self.relu = complex_relu
 
     def forward(self, inputs):
@@ -66,40 +68,43 @@ class CvnnModel(nn.Module):
         x = self.l2(x)
         x = self.relu(x)
         x = self.l3(x)
+        x = self.relu(x)
+        x = self.l4(x)
         return x
 
 ##
-def train(dataset, model):
+def train(dataset, model, num_epochs):
     model.train()
     loss_vals = []
-    num_epochs = 5
 
     dataloader = DataLoader(dataset, batch_size=64)
     loss_fn = nn.L1Loss()
-    opt = optim.Adam(model.parameters(), lr=0.001)
+    opt = optim.Adam(model.parameters())
 
     for epoch in range(num_epochs):
-        epoch_loss = 0
+        epoch_loss = []
         for x, y in dataloader:
             opt.zero_grad()
             y_pred = model(x)
             loss = loss_fn(y_pred, y)
             loss.backward()
             opt.step()
-            epoch_loss += loss.item()
-        loss_vals.append(epoch_loss)
-        print(epoch_loss)
+            epoch_loss.append(loss.item())
+        loss_vals.append(sum(epoch_loss)/len(epoch_loss))
     plt.plot(np.linspace(1, num_epochs, num_epochs).astype(int), loss_vals)
+    plt.title("L1 loss")
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
 
 
 ##
-model = CvnnModel(layer_dims=[SEQ_LEN, 10, 4])
-dataset = Dataset("data/data.npy", SEQ_LEN)
-train(dataset, model)
+model = CvnnModel(layer_dims=[SEQ_LEN, 10, 10, 4])
+dataset = Dataset(10, SEQ_LEN)
+train(dataset, model, 3)
 
 ##
 model.eval()
-x = torch.view_as_complex(torch.tensor([[45,2],[45,2],[48,4], [43,2], [42,2]], dtype=torch.float))
+x = torch.view_as_complex(torch.tensor([[65,2],[65,1],[70,4], [63,2], [68,8]], dtype=torch.float))
 model(x)
 
 ##
@@ -121,13 +126,15 @@ def to_stream(seq):
     return stream
 
 ##
-seq = generate(model, x, 50)
+x = torch.view_as_complex(torch.tensor(np.load('data/data_big_5000.npy', allow_pickle=True)[1][10:15], dtype=torch.float))
+seq = generate(model, x, 30)
+print(seq)
 stream = to_stream(seq)
 stream.show()
 
 ##
 mf = m21.midi.translate.streamToMidiFile(stream)
-mf.open('./data/test_output.midi', 'wb')
+mf.open('./data/cvnn_output.midi', 'wb')
 mf.write()
 mf.close()
 
